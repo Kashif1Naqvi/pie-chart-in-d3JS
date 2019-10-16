75ed91f279329cd545cdec22265142438740691d
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2019-10-16 15:25:12 +0500

    code updated

--- dev_02.js
+++ dev_02.js
@@ -1,6 +1,6 @@
 
 d3.json("dev_02.json").then((d,i)=>{
-  const transformedData = Object.entries(d[0]).map(([key,value]) => {
+  const transformedValue = Object.entries(d[0]).map(([key,value]) => {
     return value
   })
   const transformedKey = Object.entries(d[0]).map(([key,value]) => {
@@ -8,18 +8,22 @@ d3.json("dev_02.json").then((d,i)=>{
   })
   let width = 1000,height=800;
   // let color = d3.scaleLinear()
-  //               .domain([d3.min(transformedData),d3.max(transformedData)])
+  //               .domain([d3.min(transformedValue),d3.max(transformedValue)])
   //               .range(["#e09c09","#4a68ed","blue"])
-  var color = d3.scaleQuantize()
-    .domain([d3.min(transformedData),d3.max(transformedData)])
-    .range(["#e09c09","#4a68ed","#4451e3"]);
+  // var color = d3.scaleQuantize()
+  //   .domain([d3.min(transformedValue),d3.max(transformedValue)])
+  //   .range(["#e09c09","#4a68ed","#4451e3"]);
+  let color=[
+    {d1:"#406ac7"},
+    {d1:"red"},
+    {d1:"#f7ab07"}
+  ]
   let svg = d3.select("body").append("svg")
               .attr("width",width)
               .attr("height",height);
   let data = d3.pie().sort(null).value(function(d,i){
                 return d
-              })(transformedData);
-
+              })(transformedValue);
   let segmant = d3.arc()
                   .innerRadius(0)
                   .outerRadius(190)
@@ -31,39 +35,34 @@ d3.json("dev_02.json").then((d,i)=>{
                       .append("path")
                       .attr("d",segmant)
                       .attr("fill",function(d,i){
-                        return color(d.value)
+                        return color[i].d1;
                       })
 
   let label = d3.select("g").selectAll("text").data(data).enter()
               .append("text").each(function(d){
               let center = segmant.centroid(d);
               d3.select(this)
-                .attr("x",center[0])
+                .attr("x",center[0]-29)
                 .attr("y",center[1])
                 .attr("class","text")
-                .text(d.value/4*100 +"%")
+                .text((d.value/7*100).toFixed(1)+"%")
               }).on("mousemove",function(d){
-
-              })
-
+        })
   let legends = svg.append("g").attr("transform","translate(600,220)")
-                   .selectAll(".legeneds").data(transformedData).enter().append("g")
+                   .selectAll(".legeneds").data(transformedValue).enter().append("g")
                    .classed("legeneds",true).attr("transform",function(d,i){
                       return "translate(10,"+ (i-1) *30 + ")"
                    });
     legends.append("circle").attr("cx",15).attr("cy",10).attr("r",10)
            .attr("fill",function(d,i){
-              return color(d);
+              return  color[i].d1;
            });
     legends.append("text").text(function(d,i){
       return transformedKey[i]
     })
-    .attr("fill",function(d){
-        return color(d)
+    .attr("fill",function(d,i){
+        return  color[i].d1;
       })
     .attr("x",30)
     .attr("y",15);
-
-
-
 })
